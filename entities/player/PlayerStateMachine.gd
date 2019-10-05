0663006f52792cbe585d5c5ed9a3ca3a1181e8c9
R4NK <nahumoscatelli@gmail.com>
2019-10-05 20:06:03 -0300

    Preparado para prototipar niveles sin comportamiento

---
+++ entities/player/PlayerStateMachine.gd
@@ -0,0 +1,23 @@
+extends "res://entities/state_machine.gd"
+
+func _ready():
+	states_map = {
+		"idle": $Idle,
+		"move": $Move,
+	}
+	START_STATE = $Idle
+
+func _change_state(state_name):
+	"""
+	The base state_machine interface this node extends does most of the work
+	"""
+	if not _active:
+		return
+	._change_state(state_name)
+
+func _input(event):
+	"""
+	Here we only handle input that can interrupt states, attacking in this case
+	otherwise we let the state node handle it
+	"""
+	pass
---
+++ entities/player/states/motion/Motion.gd
@@ -0,0 +1,18 @@
+extends "res://entities/state.gd"
+
+var speed = 0.0
+var velocity = Vector2()
+
+func handle_input(event):
+	if event.is_action_pressed("simulate_damage"):
+		emit_signal("finished", "stagger")
+
+func get_input_direction():
+	var input_direction = Vector2()
+	input_direction.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	input_direction.y = int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
+	return input_direction
+
+func update_look_direction(direction):
+	if direction and owner.look_direction != direction:
+		owner.look_direction = direction
