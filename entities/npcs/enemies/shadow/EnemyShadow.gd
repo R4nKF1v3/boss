e7d4919c2f7ed5da4123c4ea8a73732c811c2371
R4NK <nahumoscatelli@gmail.com>
2019-10-24 00:52:30 -0300

    Cambios y cosas que ya no recuerdo. Nivel de prueba de concepto

--- entities/npcs/enemies/shadow/EnemyShadow.gd
+++ entities/npcs/enemies/shadow/EnemyShadow.gd
@@ -38,17 +38,24 @@ func can_see_player() -> bool:
 		var direction_to_node = (target.global_position - global_position).normalized()
 		if abs(rad2deg(Vector2(1,0).rotated(rotation).angle_to(direction_to_node))) < FOV:
 			var space_state = get_world_2d().direct_space_state
-			var target_extents = target.get_node("CollisionShape2D").shape.extents
-			var nw = target.global_position - target_extents
-			var se = target.global_position + target_extents
-			var ne = target.global_position + Vector2(target_extents.x, -target_extents.y)
-			var sw = target.global_position + Vector2(-target_extents.x, target_extents.y)
-			for pos in [target.global_position, nw, se, ne, sw]:
-				var result = space_state.intersect_ray(global_position, pos, [self], collision_mask)
-				var response = result and result.collider == target
-				if response:
-					player_last_pos = target.global_position
-					return true
+			
+			#var target_extents = target.get_node("CollisionShape2D").shape.extents
+			#var nw = target.global_position - target_extents
+			#var se = target.global_position + target_extents
+			#var ne = target.global_position + Vector2(target_extents.x, -target_extents.y)
+			#var sw = target.global_position + Vector2(-target_extents.x, target_extents.y)
+			#for pos in [target.global_position, nw, se, ne, sw]:
+			#	var result = space_state.intersect_ray(global_position, pos, [self], collision_mask)
+			#	var response = result and result.collider == target
+			#	if response:
+			#		player_last_pos = target.global_position
+			#		return true
+			
+			var result = space_state.intersect_ray(global_position, target.global_position, [self], collision_mask)
+			var response = result and result.collider == target
+			if response:
+				player_last_pos = target.global_position
+				return true
 	return false
 
 func can_reach_player():
